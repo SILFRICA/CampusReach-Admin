1b47766dbc8e5fae37c14fadf718795aa72ca1b6
DioChuks <diochuks65@gmail.com>
2024-06-27 13:03:04 +0100

    chore: added icon to the topline card

--- src/components/HomeSection.tsx
+++ src/components/HomeSection.tsx
@@ -1,15 +1,22 @@
 import React from "react";
-import { UsersIcon } from "@heroicons/react/20/solid";
+import {
+  UsersIcon,
+  BuildingOfficeIcon,
+  NewspaperIcon,
+} from "@heroicons/react/20/solid";
 
 const HomeSection: React.FC = () => {
   return (
     <div>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 mb-6 text-[#003431]">
-        <div className="bg-white rounded-md border-l-4 border-l-teal-600 p-6 shadow-md shadow-black/5">
+        <div className="bg-white rounded-md border border-l-4 border-l-teal-600 p-6 shadow-md shadow-black/5">
           <div className="flex justify-between mb-6">
             <div>
-              <div className="flex items-center mb-1">
-                <div className="text-2xl font-semibold">2</div>
+              <div className="flex items-center mb-1 gap-2">
+                <div className="text-2xl font-semibold">10</div>
+                <div className="p-1 bg-[#4dfbd9] rounded-full">
+                  <BuildingOfficeIcon className="w-6 h-6" />
+                </div>
               </div>
               <div className="text-sm font-medium">Number of Channels</div>
             </div>
@@ -22,13 +29,13 @@ const HomeSection: React.FC = () => {
             View
           </a>
         </div>
-        <div className="bg-white rounded-md border-l-4 border-l-teal-600 p-6 shadow-md shadow-black/5">
+        <div className="bg-white rounded-md border border-l-4 border-l-teal-600 p-6 shadow-md shadow-black/5">
           <div className="flex justify-between mb-4">
             <div>
               <div className="flex items-center mb-1 gap-2">
-                <div className="text-2xl font-semibold">100</div>
-                <div className="p-1 rounded bg-[#4dfbd9] rounded-full">
-                  <UsersIcon className="w-6 h-6"/>
+                <div className="text-2xl font-semibold">10000</div>
+                <div className="p-1 bg-[#4dfbd9] rounded-full">
+                  <UsersIcon className="w-6 h-6" />
                 </div>
               </div>
               <div className="text-sm font-medium">Campus Population</div>
@@ -41,10 +48,15 @@ const HomeSection: React.FC = () => {
             View
           </a>
         </div>
-        <div className="bg-white rounded-md border-l-4 border-l-teal-600 p-6 shadow-md shadow-black/5">
+        <div className="bg-white rounded-md border border-l-4 border-l-teal-600 p-6 shadow-md shadow-black/5">
           <div className="flex justify-between mb-6">
             <div>
-              <div className="text-2xl font-semibold mb-1">100</div>
+              <div className="flex items-center mb-1 gap-2">
+                <div className="text-2xl font-semibold">100</div>
+                <div className="p-1 bg-[#4dfbd9] rounded-full">
+                  <NewspaperIcon className="w-6 h-6" />
+                </div>
+              </div>
               <div className="text-sm font-medium">Number of Posts created</div>
             </div>
           </div>
